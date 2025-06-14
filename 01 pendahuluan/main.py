bfce332633c538050253bb91c57e30d375d232b1
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-06-14 22:25:22 +0700

    latihan method format python

--- 01 pendahuluan/main.py
+++ 01 pendahuluan/main.py
@@ -1,1 +1,89 @@
 print("hello world")
+
+# menggunakan kapital
+x = "hello world"
+x = x.capitalize()
+print(x)
+
+# penggunaan isalnum
+y = "hello123"
+y = y.isalnum()
+print(y)
+
+# penggunaan isalpha
+z = "hello"
+z = z.isalpha()
+print(z)
+
+# penggunaan islower
+a = "hello"
+a = a.islower()
+print(a)
+
+# isnumeric
+b = "12345"
+b = b.isnumeric()
+print(b)
+
+# penggunaan isspace
+c = "   "
+c = c.isspace()
+print(c)
+
+# penggunaan isupper
+d = "HELLO"
+d = d.isupper()
+print(d)
+
+# penggunaan max
+e = "hello"
+f = "world"
+print(max(e, f))
+
+# penggunaan min
+g = "hello"
+h = "world"
+print(min(g, h))
+
+# penggunaan replace
+i = "hello world"
+i = i.replace("world", "python")
+print(i)
+
+# pengunaan split
+j = "hello world"
+j = j.split()
+print(j)
+
+# penggunaan strip
+k = "   hello world   "
+k = k.strip()
+print(k)
+
+# penggunaan join(sequence)
+l = ["hello", "world"]
+l = " ".join(l)
+print(l)
+
+# penggunaan count
+m = "hello world"
+m = m.count("o")
+print(m)
+
+# penggunaan len
+n = "hello world"
+n = len(n)
+print(n)
+
+# penggunaan lower
+o = "hello"
+o = o.lower()
+print(o)
+# penggunaan upper
+p = "HELLO"
+p = p.upper()
+print(p)
+# penggunaan isdigit
+q = "12345"
+q = q.isdigit()
+print(q)
---
+++ 01 pendahuluan/symbol.py
@@ -0,0 +1,19 @@
+print("======= PENGGUNAAN SYMBOL =======  \n")
+
+# penggunaan %c
+print("Karakter pertama: %c" % 'B')
+
+# penggunaan %s
+print("Nama saya %s" % 'Budi')
+
+# penggunaan %d
+print("Umur saya %d tahun" % 20)
+
+# penggunaan %f
+print("Nilai rata-rata saya %.2f" % 85.5)
+
+# penggunaan %x
+print("Nilai heksadesimal dari 255 adalah %x" % 255)
+
+# penggunaan %o
+print("Nilai oktal dari 255 adalah %o" % 255)
