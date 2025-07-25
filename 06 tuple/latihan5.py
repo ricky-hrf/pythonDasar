82fb3bb7cbe4ed907104cf2d03e8a758d6c6a200
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-07-25 19:12:29 +0700

    Latihan Tuple

---
+++ 06 tuple/latihan5.py
@@ -0,0 +1,11 @@
+DataBarang = (("Beras", 10000), ("Gula", 12000), ("Minyak Goreng", 15000))
+searchProduk = input("Masukkan nama produk yang ingin dicari: ")
+# found = False
+for barang, harga in DataBarang:
+    if searchProduk.lower() == barang.lower():
+        print(f"Barang: {barang}, Harga: {harga:,}".replace(',', '.'))
+        break
+        # found = True
+# if not found:
+else:
+    print("Produk tidak ditemukan.")
